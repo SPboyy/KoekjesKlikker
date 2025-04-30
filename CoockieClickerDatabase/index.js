ec4e7df79b447758b72fe4486901078b33b25e8c
GeoffreyCompernolle <geoffrey.compernolle@student.vives.be>
2025-04-30 17:11:23 +0200

    db connection

--- CoockieClickerDatabase/index.js
+++ CoockieClickerDatabase/index.js
@@ -1,15 +1,37 @@
+const express = require('express');
+const app = express();
+const mssql = require("mssql");
 
-var mysql = require('mysql');
+app.get('/', function (req, res) {
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1234567890",
-  database: 'CoockieClicker',
-  port: 3306,
+  // Config your database credential
+  const config = {
+      user: 'root',
+      password: '1234567890',
+      server: 'localhost',
+      database: 'CoockieClicker'
+  };
+   // Connect to your database
+   mssql.connect(config, function (err) {
+
+    // Create Request object to perform
+    // query operation
+    let request = new mssql.Request();
+
+    // Query to the database and get the records
+    request.query('select * from Player',
+        function (err, records) {
+
+            if (err) console.log(err)
+
+            // Send records as a response
+            // to browser
+            res.send(records);
+
+        });
+});
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+let server = app.listen(5000, function () {
+console.log('Server is listening at port 5000...');
 });
