09331dbb6cd68ae6ed64c347fa3ce4dd6958d124
Seppe De Brauwere <seppe.de.brauwere@gmail.com>
2025-04-30 10:00:48 +0200

    handlebars (fingerscrossed da het werkt)

---
+++ app.js
@@ -0,0 +1,27 @@
+const port = process.env.PORT || 3000;
+
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const app = express();
+const bodyParser = require('body-parser');
+
+// configure Handlebars view engine
+app.engine("handlebars", expressHandlebars.engine({
+    defaultLayout: "main",
+}));
+app.set("view engine", "handlebars");
+
+app.use(express.static(__dirname + "/public"));
+// custom 404 page
+app.use((req, res) => {
+    res.render("errors/404");
+})
+// custom 500 page
+app.use((err, req, res, next) => {
+    console.error(err.message);
+    res.render("errors/500");
+});
+app.listen(port, () => console.log(
+    `Express started on http://localhost:${port};  `+
+    `press Ctrl-C to terminate.`));
+
