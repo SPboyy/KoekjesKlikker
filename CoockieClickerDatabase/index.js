5b2747b06ca5975bccaec927d0efbba2dca204f9
GeoffreyCompernolle <geoffrey.compernolle@student.vives.be>
2025-04-30 11:59:16 +0200

    database toegevoegd

---
+++ CoockieClickerDatabase/index.js
@@ -0,0 +1,15 @@
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234567890",
+  database: 'CoockieClicker',
+  port: 3306,
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
