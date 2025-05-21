cd2c3f83996f1dd400244e08f7e4fbf234791c87
Seppe <seppe.de.brauwere@gmail.com>
2025-05-21 11:09:23 +0200

    prestige data

--- assets/data/Prestige.js
+++ assets/data/Prestige.js
@@ -1,1 +1,14 @@
-const PrestigeUpgrades = []
+const PrestigeUpgrades = [
+    {PrestigeID: 1,PrestigeName: "first boost", PrestigeDescription: "gain a 5% cookie boost", price: 1, available: 1, unlocked: 0},
+    {PrestigeID: 2,PrestigeName: "getting a discount", PrestigeDescription: "buildings are 10% cheaper", price: 5, available: 1, unlocked: 0},
+    {PrestigeID: 3,PrestigeName: "getting a head start", PrestigeDescription: "start with 10 rolling pins", price: 10, available: 1, unlocked: 0},
+    {PrestigeID: 4,PrestigeName: "starter gambler", PrestigeDescription: "gain the ability to gamble with the cookie monster", price: 10, available: 1, unlocked: 0},
+    {PrestigeID: 5,PrestigeName: "Even more cookies", PrestigeDescription: "gain an additional 5% cookie boost", price: 10, available: 1, unlocked: 0},
+    {PrestigeID: 6,PrestigeName: "discounting it all", PrestigeDescription: "upgrades are 10% cheaper", price: 15, available: 1, unlocked: 0},
+    {PrestigeID: 7,PrestigeName: "great start", PrestigeDescription: "start with 5 cookie monsters", price: 25, available: 1, unlocked: 0},
+    {PrestigeID: 8,PrestigeName: "feelin lucky", PrestigeDescription: "the odds in the cookie monster gambling mini game are increased", price: 50, available: 1, unlocked: 0},
+    {PrestigeID: 9,PrestigeName: "feeling sleepy", PrestigeDescription: "ofline time is increased with an hour", price: 15, available: 1, unlocked: 0},
+    {PrestigeID: 10,PrestigeName: "Cookie overload", PrestigeDescription: "gain an aditional 15% cookie boost", price: 25, available: 1, unlocked: 0},
+    {PrestigeID: 11,PrestigeName: "afk grind", PrestigeDescription: "ofline increased with 2 hours", price: 1, available: 30, unlocked: 0},
+    {PrestigeID: 12,PrestigeName: "running out of storage", PrestigeDescription: "gain a 500% cookie boost", price: 999, available: 1, unlocked: 0},
+]
